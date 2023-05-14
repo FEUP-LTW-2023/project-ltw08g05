2d7ace566207e07d8b208bd5d2f0b7f89c190cef
Spleentery <davidburchakov@gmail.com>
2023-05-14 20:53:38 +0100

    Hamburger finally fixed!

--- src/controllers/action_edit_user.php
+++ src/controllers/action_edit_user.php
@@ -4,12 +4,13 @@ require_once '../models/Musers.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // get new data
-    $id = $_POST['id'];
-    $title = $_POST['title'];
+    $email = $_POST['email'];
+    $first_name = $_POST['first_name'];
     $content = $_POST['content'];
 
     $db = getDatabaseConnection();
 
+    $user = User::getUserByEmail($db, $email);
     $ticket = Ticket::getTicket($db, $id);
     if ($ticket) {
         $ticket->title = $_POST['title'];
